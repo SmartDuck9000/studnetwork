667e62f8045346de2cdf27f46546ea9476fb67c6
Georg <gitSpam404@gmail.com>
2020-08-08 21:56:29 +0300

    #1 add project files

---
+++ studnetwork/studnetwork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  studnetwork
+//
+//  Created by Георгий Куликов on 21.07.2020.
+//  Copyright © 2020 Георгий Куликов. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
