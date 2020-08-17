828d2eed28e4557fe55cd31b0107f911c56334a5
Georg <gitSpam404@gmail.com>
2020-08-18 00:38:55 +0300

    #4 prepare user struct for parsing user data

---
+++ studnetwork/studnetwork/Profile/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  studnetwork
+//
+//  Created by Георгий Куликов on 18.08.2020.
+//  Copyright © 2020 Георгий Куликов. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    var token: String
+}
